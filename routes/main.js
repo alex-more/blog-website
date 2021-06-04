29a7f55446bf33f4cacc279dfcc077203df0ceb9
ClickheadZ <84818782+morax-light@users.noreply.github.com>
2021-06-03 20:01:31 -0400

    Routing completed

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,22 @@ const express = require('express')
 const Article = require('./../models/article')
 const router = express.Router()
 
+
+router.get('/about', async (req, res) => {
+    res.render('about')
+})
+
+router.get('/projects', async (req, res) => {
+    res.render('projects')
+})
+
 router.get('/blog', async (req, res) => {
     const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/blog', { articles: articles })
+})
+
+router.get('/contact', async (req, res) => {
+    res.render('contact')
 })
 
 module.exports = router
